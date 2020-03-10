7e35afb6f7878930bd0b535c0e52fbef40d94387
x <x@xs-MacBook-Pro.local>
2020-03-10 17:10:26 -0500

    Updates

---
+++ precache-manifest.4aab3c55a69bef1edaf9a849d9648d09.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf11bdf892a3b448d5fac5cce4269b01",
+    "url": "/react_blog_example/index.html"
+  },
+  {
+    "revision": "5e43fd695190d8b96077",
+    "url": "/react_blog_example/static/js/2.98930ebf.chunk.js"
+  },
+  {
+    "revision": "e2402dee9201414959d7",
+    "url": "/react_blog_example/static/js/main.08c71bea.chunk.js"
+  },
+  {
+    "revision": "40b04f7673d2f9c21a5f",
+    "url": "/react_blog_example/static/js/runtime-main.a5f0b2e5.js"
+  }
+]);
